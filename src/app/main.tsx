3cecf312df40cced6bdfc445475ba3626bdafadc
Max Pavlov <m.n.pavlov@gmail.com>
2026-02-08 18:40:53 +0500

    feat: add react-router and configure basename for gh-pages

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 1. Импортируем Роутер
 import { App } from './App.tsx';
 import './styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/maksim-pavlov-portfolio">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
